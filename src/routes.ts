989a91770e0b12cbeb81537901a2b6607f11c38f
Leonard Parisi <leonardparisi@testbox.com>
2022-11-15 13:37:13 -0500

    Updated message list delimiter

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,12 @@
+import { logger } from "./helpers/logging";
 import { send_text } from "./helpers/send_text";
 
 export function setup_routes(app: any) {
   app.post("/send_text", (req: any, res: any) => {
+    logger.info("Send text recieved", { body: req.body });
     const { phone_number, message, minute_variance, date_to_send } = req.body;
-    send_text(
-      phone_number,
-      Array.isArray(message) ? message : [message],
-      minute_variance,
-      date_to_send
-    );
+    const message_options = message.split(" * ");
+    send_text(phone_number, message_options, minute_variance, date_to_send);
     res.send("ok");
   });
 
